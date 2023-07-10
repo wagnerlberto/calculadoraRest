487d63d48f93690711f2c173851d2b065c6a2c9b
prof. Wagner Berto <54516340+profwagnerberto@users.noreply.github.com>
2023-07-09 22:05:46 -0300

    first

---
+++ src/main/java/br/com/empresa/calculadoraRest/CalculadoraRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.empresa.calculadoraRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraRestApplication.class, args);
+	}
+
+}
